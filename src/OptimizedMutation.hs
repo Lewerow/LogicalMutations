ee2df7ce613814dc0499c28d6c044e2ac7a06089
Tomasz Lewowski <tomasz.lewowski@wp.eu>
2016-10-13 23:59:28 +0200

    Tryin' to add a monad

--- src/OptimizedMutation.hs
+++ src/OptimizedMutation.hs
@@ -2,11 +2,32 @@ module OptimizedMutation where
 
 import qualified Data.Set as Set
 import Control.Monad (replicateM)
+import Control.Monad.State
 
 import Language
 import Treefication
 import Evaluation
 import VariableHelpers
+
+data OptimizedMutationConfiguration = OMC
+data OptimizedMutationReport = OMR
+data SingleMutantReport = SMR
+
+data Mutation = Mutation {
+  mutatedNode :: NodeId,
+  expressionVersion :: Int
+}
+
+data MutationProcedure a = MP {
+  runMutation :: a
+}
+
+type MutationState = StateT MutationState MutationProcedure
+
+instance Monad MutationProcedure MutationState where
+  a >>= b = 
+  a >> b = b
+  return a = MP a
 
 
 generateInputSpace :: Expression -> [[(Variable, Expression)]]
@@ -15,3 +36,7 @@ generateInputSpace expr = inputSequence where
     variablesCount = length variables
     availableValues = replicateM variablesCount [Operand Truth, UnaryOperator Not (Operand Truth)]
     inputSequence = map (\p -> zip (fst p) (snd p)) $ zip (repeat variables) availableValues
+
+
+analyzeMutant :: OptimizedMutationConfiguration -> StructuredExpression -> [Mutation] -> State
+analyzeMutant conf cache expr mutations =
